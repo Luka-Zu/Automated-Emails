cb78608d131104fc3d7ef704889e4553e4b4a56b
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-22 17:45:56 +0400

    .

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# 9646d008451e4212b437cb0f9805d16d
+import  requests
+from pprint import pprint
 class NewsFeed:
 
     def __init__(self, data):
@@ -5,3 +8,7 @@ class NewsFeed:
 
     def get(self):
         pass
+
+response = requests.get("https://newsapi.org/v2/everything?q=tesla&from=2022-07-22&sortBy=publishedAt&apiKey=9646d008451e4212b437cb0f9805d16d")
+content = response.text
+pprint(content)
