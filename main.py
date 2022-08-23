e3af808284fa7beaebb0e5123cb883104f455cd8
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-24 00:21:04 +0400

    FINISHED

--- main.py
+++ main.py
@@ -5,30 +5,32 @@ from news import NewsFeed
 import datetime
 
 
+def send_email():
+
+    date_string = datetime.datetime.now().strftime('%y-%m-%d')
+    news_feed = NewsFeed(interest=row['interest'],
+                         from_date=date_string,
+                         to_date=date_string)
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server.login(user="zukhbaialuka0@gmail.com", password="incorrect_password")
+    message = f"Hi {row['name']}\n" \
+              f"See today's news about {row['interest']}" \
+              f"{news_feed.get()}"
+    # .replace(u"\u2018", "'").replace(u"\u2013", "'").replace(u"\u2019", "'").replace(
+    # u"\u2014", "'").replace(u"\u2015", "'").replace(u"\u2020", "'").replace(u"\u201c", "'").replace(u"\u201d", "'")
+    server.sendmail(from_addr="zukhbaialuka0@gmail.com",
+                    to_addrs=row['email'],
+                    msg=message)
+    server.quit()
+
+
 while True:
     if datetime.datetime.now().hour == 23 and datetime.datetime.now().minute == 59:
         df = pandas.read_excel('people.xlsx')
 
         for index, row in df.iterrows():
 
-            date_string = datetime.datetime.now().strftime('%y-%m-%d')
-            news_feed = NewsFeed(interest=row['interest'],
-                                 from_date=date_string,
-                                 to_date=date_string)
-
-            server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-            server.login(user="zukhbaialuka0@gmail.com", password="incorrect_password")
-
-            message = f"Hi {row['name']}\n" \
-                      f"See today's news about {row['interest']}" \
-                      f"{news_feed.get()}"
-                # .replace(u"\u2018", "'").replace(u"\u2013", "'").replace(u"\u2019", "'").replace(
-                # u"\u2014", "'").replace(u"\u2015", "'").replace(u"\u2020", "'").replace(u"\u201c", "'").replace(u"\u201d", "'")
-
-            server.sendmail(from_addr="zukhbaialuka0@gmail.com",
-                            to_addrs=row['email'],
-                            msg=message)
-            server.quit()
+            send_email()
         time.sleep(60)
 
 # FUCK yagmail library !
--- news.py
+++ news.py
@@ -19,16 +19,9 @@ class NewsFeed:
         self.interest = interest
 
     def get(self):
-        url = f"{self.base_url}" \
-              f"qInTitle={self.interest}&" \
-              f"from={self.from_date}&" \
-              f"to={self.to_date}&" \
-              f"language={self.language}&" \
-              f"apiKey={self.api_key}"
+        url = self._build_url()
 
-        response = requests.get(url)
-        content = response.json()
-        articles = content['articles']
+        articles = self._get_articles(url)
         # print(x)
         # pprint(content)
 
@@ -37,3 +30,19 @@ class NewsFeed:
             email_body = email_body + article['title'] + '\n' + article['url'] + '\n\n'
 
         return email_body
+
+    def _get_articles(self, url):
+        response = requests.get(url)
+        content = response.json()
+        articles = content['articles']
+        return articles
+
+    def _build_url(self):
+        url = f"{self.base_url}" \
+              f"qInTitle={self.interest}&" \
+              f"from={self.from_date}&" \
+              f"to={self.to_date}&" \
+              f"language={self.language}&" \
+              f"apiKey={self.api_key}"
+
+        return url
